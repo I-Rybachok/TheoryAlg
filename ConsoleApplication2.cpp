8f7a39d71cce7bc8e81c7172777d33f5f3207c17
Ірина Рибачок <113577981+I-Rybachok@users.noreply.github.com>
2023-09-19 16:25:36 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,37 @@
+﻿#include <math.h> 
+#include <iostream>
+using namespace std;
+
+// приклад поліному: y = 2 * x^3 + 3 * x^2 + 4 * x + 5, x = 2
+
+int main()
+{
+    float x;
+    int n;
+    int  y = 0;
+
+    cout << "Enter n : "; cin >> n; cout << endl;
+
+    int* a = new int[n + 1];
+
+    cout << "Enter x : "; cin >> x; cout << endl;
+
+    for (int i = 0; i <= n; ++i)
+    {
+        cout << "Enter a" << i << " : "; cin >> a[i]; cout << endl;
+    }
+
+    int n1 = n;
+    y = a[n1]; 
+
+    for (int i = 0; i < n1; ++i)
+    {
+        y += a[i] * pow(x,n);
+        n--;
+    }
+    cout << "Polynom's result = " << y; cout << endl;
+    system("pause");
+
+    return 0;
+}
+
